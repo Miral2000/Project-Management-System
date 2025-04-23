4b28a90bb093a4b313d510b6f215a01624327ad9
lamiaalill <sagtme632000@gmail.com>
2025-04-23 21:45:49 +0200

    Add files via upload
    upload by lamia ali

--- main.cpp
+++ main.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 #include <string>
-#include <stdexcept>
 using namespace std;
 
-int main() {
-    string correctUsername = "miral";
-    string correctPassword = "456";
-    string username, password;
 
-    cout << "Login Page" << endl;
-    try {
-        cout << "Enter username: ";
-        cin >> username;
-        cout << "Enter password: ";
-        cin >> password;
+class project {
+public:
+    string name, startdate, enddate, description;
 
-        if (username == correctUsername && password == correctPassword) {
-            cout << "Login success!" << endl;
-        } else {
-            throw runtime_error("Invalid username or password.");
+    void input() {
+        try {
+            cout << "enter the name of project: ";
+            getline(cin, name);
+            cout << "enter the start date: ";
+            getline(cin, startdate);
+            cout << "enter the end date: ";
+            getline(cin, enddate);
+            cout << "enter the description: ";
+            getline(cin, description);
         }
-    } catch (const exception& e) {
-        cout << "Login failed: " << e.what() << endl;
+        catch (const exception& e) {
+            cout << "the are error: " << e.what() << endl;
+        }
+    }
+
+    void display() {
+        try {
+            cout << "\nthe name of project is: " << name << endl;
+            cout << "the description of project is: " << description << endl;
+            cout << "the start date of project is: " << startdate << endl;
+            cout << "the end date of project is: " << enddate << endl;
+        }
+        catch (const exception& e) {
+            cout << "the are error: " << e.what() << endl;
+        }
+    }
+};
+
+int main() {
+    try {
+
+        project pro[100];
+        int c = 0;
+        char x = 'y';
+
+        while ((x == 'y' || x == 'Y')){
+            pro[c].input();
+            c++;
+
+            cout << "do you want to add another project? (y/n): ";
+            cin >> x;
+            cin.ignore();
+        }
+
+
+        for (int i = 0; i < c; i++) {
+                cout<<"the information of  project "<<i+1<<" is:\n";
+            pro[i].display();
+        }
+    }
+    catch (const exception& e) {
+        cout << "\n the are error in the program: " << e.what() << endl;
     }
 
     return 0;
